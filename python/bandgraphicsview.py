469a1e036d12047e4ec61f0055352ea57ae40e38
Sreeraj Rajendran <rsreeraj@gmail.com>
2012-06-29 01:18:45 +0530

    Added aligignment, autofit and scaling to band-diagram

--- python/bandgraphicsview.py
+++ python/bandgraphicsview.py
@@ -1,8 +1,10 @@
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui, QtCore, Qt
 
 class BandGraphicsView(QtGui.QGraphicsView):
     def resizeEvent(self, event):
-        pass
 #print event.size().width()
 #self.centerOn(QtCore.QPointF(100,100))
-#self.fitInView(self.scene().itemsBoundingRect(), QtCore.Qt.KeepAspectRatio)
+        self.setAlignment(Qt.Qt.AlignCenter)
+        self.fitInView(self.scene().itemsBoundingRect(), QtCore.Qt.KeepAspectRatio)
+        self.scale(1.3,1.3)
+        self.setViewportMargins(10,10,10,10)
